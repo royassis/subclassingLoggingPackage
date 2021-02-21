ded063ed82186f5012fe70ccfc4bc5faa4dcaae3
Roy <royassisg@gmail.com>
2021-02-22 00:39:50 +0200

    Commit

--- CustomHandlers.py
+++ CustomHandlers.py
@@ -1,21 +1,35 @@
 import logging
 import pymongo
 
-
 class MongoHandler(logging.Handler):
     def __init__(self,host, db, collection ):
         super().__init__()
         self.host = host
         self.db = db
         self.collection = collection
+        self.myclient= None
 
     def emit(self, record):
         try:
             myclient = pymongo.MongoClient(f"mongodb://{self.host}/", serverSelectionTimeoutMS= 1000)
+            self.myclient = myclient
             mycol = myclient[self.db][self.collection]
             # msg = self.format(record)
             mycol.insert_one(record.__dict__)
-            myclient.close()
-        except Exception as e:
-            print('CRITICAL DB ERROR! Logging to database not possible!:')
-            print(e)
+            # myclient.close()
+        except:
+            self.handleError(record)
+
+    def handleError(self, record):
+        if logging.raiseException:
+            logging.exception("Could not write to mongo")
+        else:
+            pass
+
+
+    def close(self):
+        try:
+            self.myclient.close()
+            logging.info("Closing connection of logger to MongoDB")
+        except AttributeError:
+            pass
--- main.py
+++ main.py
@@ -3,27 +3,40 @@ from CustomHandlers import MongoHandler
 import socket
 from Messages import Message
 
-hostname = socket.gethostname()
-internal_ip = socket.gethostbyname(socket.gethostname())
-
 logging.basicConfig(
-            format='%(asctime)s %(levelname)s %(message)s - %(internal_ip)s - %(host)s',
+            format='%(asctime)s %(levelname)s %(message)s',
             level=logging.INFO,
             datefmt='%m/%d/%Y %I:%M:%S %p'
             )
 
+logging.raiseException = False
+
+hostname = socket.gethostname()
+internal_ip = socket.gethostbyname(socket.gethostname())
+
 my_logger = logging.getLogger('mongo-logger')
 my_logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(host)s')
+stream_handler.setFormatter(stream_formatter)
+
+mongo_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 mongo_handler = MongoHandler("localhost:27017", "testdb", "testcollection")
-mongo_handler.setFormatter(formatter)
+mongo_handler.setFormatter(mongo_formatter)
+
+my_logger.addHandler(stream_handler)
 my_logger.addHandler(mongo_handler)
 
+
 msg = "Another message"
-payload = Message(machine= 2,
+payload = Message(machine= 22,
                   snif= 2,
-                  success= True,
+                  success= False,
                   host= hostname,
                   internal_ip= internal_ip)
 
+
 my_logger.info(msg, extra = payload.as_dict())
+
+
