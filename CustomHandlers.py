8c00171b098decb2134c40e3d845885f08f6efbb
royassis <34189329+royassis@users.noreply.github.com>
2022-01-05 11:14:01 +0200

    Update CustomHandlers.py
    add more custom handlers

--- CustomHandlers.py
+++ CustomHandlers.py
@@ -1,5 +1,11 @@
 import logging
 import pymongo
+from datetime import datetime
+from logging import FileHandler
+from pathlib import Path
+from logging.handlers import RotatingFileHandler
+
+from helpers import rename_logfiles
 
 
 class MongoHandler(logging.Handler):
@@ -26,3 +32,28 @@ class MongoHandler(logging.Handler):
             self.internal_logger.exception("Could not write to mongo")
         else:
             pass
+
+        
+class PerRunFileHandler(FileHandler):
+    def __init__(self, dirname, strftime="%Y-%m-%dT%H:%M:%S"):
+        dirname = Path(dirname)
+        dirname.mkdir(parents=True, exist_ok=True)
+        filename = datetime.now().strftime(strftime) + ".log"
+        filepath = dirname.joinpath(filename)
+        super().__init__(filepath, mode="a")
+
+
+class CustomFileHandler(FileHandler):
+    def __init__(self, dirname, filename):
+        dirname = Path(dirname)
+        dirname.mkdir(parents=True, exist_ok=True)
+        filepath = dirname.joinpath(filename)
+        filename = Path(filename)
+        rename_logfiles(dirname, filename)
+        super().__init__(filepath, mode="a")
+
+
+class CustomRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, filename, mode='a', maxBytes=0, backupCount=0, encoding=None, delay=False):
+        Path(filename).parent.mkdir(parents=True, exist_ok=True)
+        super().__init__(filename, mode, maxBytes, backupCount, encoding, delay)
