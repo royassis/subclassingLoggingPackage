a484112be91bcd7e11184b17a293c2e17f234cea
roya <roya@cgen.com>
2022-01-05 11:32:57 +0200

    cmt

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from setuptools import setup
+from pathlib import Path
+
+from setuptools import setup, find_packages
+from src.formatConverterCli import __version__
+
+THIS_DIR = Path(__file__).parent
+
+REQUIREMENTS_FILE = THIS_DIR.joinpath("requirements.txt")
+
+install_requires = []
+with open(REQUIREMENTS_FILE) as fh:
+    for line in fh:
+        install_requires.append(line.strip("\n"))
 
 setup(
-    name='customHandlers',
-    version='0.0.1',
-    packages=[''],
-    url='',
-    license='',
-    author='roya',
-    author_email='roya@cgen.com',
-    description=''
+    name="customHandlers",
+    version=__version__,
+    author="Roy Assis",
+    author_email="roya@cgen.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=install_requires,
+    include_package_data=True,
+    package_dir={"": "customHandlers"},
+    packages=find_packages(where="customHandlers"),
 )
