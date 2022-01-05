aea47facf1fea4c95cfa9718584ce6e7e7eaa006
roya <roya@cgen.com>
2022-01-05 11:35:21 +0200

    cmt

--- customHandlers/test_handlers.py
+++ customHandlers/test_handlers.py
@@ -1,6 +1,12 @@
 import logging
 import unittest
 from pathlib import Path
+import logging
+import socket
+from Messages import Message
+from logging.config import dictConfig
+import yaml
+
 
 from myhandlers import CustomRotatingFileHandler, CustomFileHandler
 
@@ -34,3 +40,29 @@ class TestCustomLoggers(unittest.TestCase):
 
         for _ in range(10):
             logger.error('This is an error')
+
+    def test_MongoHandler(self):
+        with open('logging_config_files/logging_config.yaml', 'rt') as file:
+            config = yaml.safe_load(file.read())
+            dictConfig(config)
+
+        # Variables
+        hostname = socket.gethostname()
+        internal_ip = socket.gethostbyname(socket.gethostname())
+
+        # Configs
+        logging.raiseExceptions = False
+
+        # Main logger - mongo and console
+        my_logger = logging.getLogger('mongo-logger')
+
+        payload = Message(machine=22,
+                          snif=2,
+                          success=False,
+                          host=hostname,
+                          internal_ip=internal_ip)
+
+        my_logger.info("some info message", extra=payload.as_dict())
+        my_logger.error("some warning message", extra=payload.as_dict())
+
+
